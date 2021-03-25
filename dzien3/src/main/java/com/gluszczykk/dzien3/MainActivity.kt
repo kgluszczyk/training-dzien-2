a5c97255a4647bab71cb979e7b2f15f0e770a4e2
Krzysztof Gluszczyk <gluszczykk@ryanair.com>
2021-03-25 14:45:38 +0100

    Display fragment dynamically

--- dzien3/src/main/java/com/gluszczykk/dzien3/MainActivity.kt
+++ dzien3/src/main/java/com/gluszczykk/dzien3/MainActivity.kt
@@ -2,7 +2,13 @@ package com.gluszczykk.dzien3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.annotation.DrawableRes
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 
@@ -14,8 +20,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
         viewModel.actionStream.observe(this, Observer<Brightness> {
-            Toast.makeText(this, "Kliknięto: $it", Toast.LENGTH_SHORT).show()
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment2, DetailsFragment.newInstance(it.imageSrc))
+                .commit()
         })
         setContentView(R.layout.activity_main)
+    }
+}
+
+class DetailsFragment : Fragment() {
+
+    companion object {
+        private const val ImageSrcKey = "ImageSrcKey"
+        fun newInstance(@DrawableRes imageSrc: Int): Fragment {
+            val args = Bundle()
+            args.putInt(ImageSrcKey, imageSrc)
+
+            val fragment = DetailsFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return ImageView(requireContext()).apply {
+            setImageResource(requireArguments().getInt(ImageSrcKey))
+        }
     }
 }

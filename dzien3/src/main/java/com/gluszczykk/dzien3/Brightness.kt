08e11d8a409e91aa5809826db234e70ddedf20c3
Krzysztof Gluszczyk <gluszczykk@ryanair.com>
2021-03-25 11:12:01 +0100

    Recycler view

---
+++ dzien3/src/main/java/com/gluszczykk/dzien3/Brightness.kt
@@ -0,0 +1,5 @@
+package com.gluszczykk.dzien3
+
+import androidx.annotation.DrawableRes
+
+data class Brightness(val name: String, @DrawableRes val imageSrc :Int)
---
+++ dzien3/src/main/java/com/gluszczykk/dzien3/BrightnessRepository.kt
@@ -0,0 +1,9 @@
+package com.gluszczykk.dzien3
+
+fun getBrightnesses() = listOf(
+    Brightness(name = "100%", imageSrc = R.drawable.ic_baseline_brightness_100),
+    Brightness(name = "80%", imageSrc = R.drawable.ic_baseline_brightness_80),
+    Brightness(name = "60%", imageSrc = R.drawable.ic_baseline_brightness_60),
+    Brightness(name = "40%", imageSrc = R.drawable.ic_baseline_brightness_40),
+    Brightness(name = "20%", imageSrc = R.drawable.ic_baseline_brightness_20)
+)

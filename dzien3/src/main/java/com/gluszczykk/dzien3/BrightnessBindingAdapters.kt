4ecb8c0149328f5d039e8c9a93955aed5cf5533d
Krzysztof Gluszczyk <gluszczykk@ryanair.com>
2021-03-26 13:55:13 +0100

    Glide

--- dzien3/src/main/java/com/gluszczykk/dzien3/BrightnessBindingAdapters.kt
+++ dzien3/src/main/java/com/gluszczykk/dzien3/BrightnessBindingAdapters.kt
@@ -3,13 +3,16 @@ package com.gluszczykk.dzien3
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 
 object BrightnessBindingAdapters {
 
     @JvmStatic
     @BindingAdapter("itemModel")
     fun setBrightnessImage(imageView: ImageView, brightness: Brightness){
-        // TODO: 26/03/2021 Zmigruj
-        //imageView.setImageResource(brightness.imageSrc)
+        Glide.with(imageView)
+            .load(brightness.imageSrc)
+            .centerCrop()
+            .into(imageView)
     }
 }

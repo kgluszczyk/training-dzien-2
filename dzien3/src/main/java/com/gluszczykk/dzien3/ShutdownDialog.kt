10ce73d45332e2b8f39a1be910b6f244e6e96931
Krzysztof Gluszczyk <gluszczykk@ryanair.com>
2021-03-26 11:39:07 +0100

    Dialogi v2

--- dzien3/src/main/java/com/gluszczykk/dzien3/ShutdownDialog.kt
+++ dzien3/src/main/java/com/gluszczykk/dzien3/ShutdownDialog.kt
@@ -2,12 +2,16 @@ package com.gluszczykk.dzien3
 
 import android.app.Dialog
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import com.gluszczykk.dzien3.databinding.FormBinding
 
 class ShutdownDialog : DialogFragment() {
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+   /* override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return AlertDialog.Builder(requireContext())
             .setTitle("Zamknij aplikacje")
             .setMessage("Jesteś pewien?")
@@ -16,5 +20,15 @@ class ShutdownDialog : DialogFragment() {
             .setNeutralButton("Nie wiem") { _, _ -> }
             .setNegativeButton("Nie!") { dialog, _ -> dialog.dismiss() }
             .create()
+    }*/
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = FormBinding.inflate(inflater)
+        //Alternatywnie
+        // val view = inflater.inflate(R.layout.form,container)
+        binding.submit.setOnClickListener {
+            dialog?.dismiss()
+        }
+        return binding.root
     }
 }
